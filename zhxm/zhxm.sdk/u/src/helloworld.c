33737f39624d451d4ac037e4b4e0cda016fc8d62
liyineng <liyineng@github.com>
2026-06-10 17:46:18 +0800

    revert: restore to fb091d8 (two versions back)

--- zhxm/zhxm.sdk/u/src/helloworld.c
+++ zhxm/zhxm.sdk/u/src/helloworld.c
@@ -361,7 +361,13 @@ u32 calc_load_value(u32 freq_hz)
 
     if(freq_hz > 20000) freq_hz = 20000;
 
-    timer_cnt = TIMER_CLK_HZ / (freq_hz * SAMPLES_PER_CYCLE);
+    if(sw & 0x80000000)
+
+        timer_cnt = TIMER_CLK_HZ / (freq_hz * 256);
+
+    else
+
+        timer_cnt = TIMER_CLK_HZ / (freq_hz * 128);
 
     if(timer_cnt < 2) timer_cnt = 2;
 
@@ -415,6 +421,10 @@ void process_fixed_frame(u8 cmd_ch, u8* data)
             sw &= ~0x80000000;
 
         sw &= ~0x40000000;
+
+        new_freq = freq_hz_a;
+
+        freq_update = 1;
 
         return;
 
@@ -616,7 +626,7 @@ void T0Handler(void)
 
                 u8 ov_b = (rv_b * amplitude_b) >> 7;
 
-                dac_write_fast(CHB_CMD, ov_b);
+                dac_write_fast(BUF_CMD, ov_b);
 
                 sw |= 0x40000000;
 
@@ -766,10 +776,6 @@ int main(void)
 {
 
     arb_table_init();
-
-    new_freq   = DEFAULT_FREQ_HZ;
-
-    freq_update = 0;
 
     spi_init();
 
