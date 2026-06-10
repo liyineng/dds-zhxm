b497c82c2a054ecfb672c07dcbc096d54fb0dcc8
liyineng <liyineng@github.com>
2026-06-10 17:09:53 +0800

    feat: syncon dual-channel via timer double-rate + alternating single SPI write
    - sync_mode=1: timer uses 256 samples/cycle (double rate)
    - Even ISRs: BUF_CMD loads CHB into DAC buffer
    - Odd ISRs:  CHA_CMD writes CHA + pushes buffer to CHB
    - Each ISR does EXACTLY 1 SPI write (same as original)
    - sync_mode=0: original single CHA at 128 samples/cycle
    - FCMD_SYNC triggers timer recalculation + phase reset

--- zhxm/zhxm.sdk/u/src/helloworld.c
+++ zhxm/zhxm.sdk/u/src/helloworld.c
@@ -361,7 +361,13 @@ u32 calc_load_value(u32 freq_hz)
 
     if(freq_hz > 20000) freq_hz = 20000;
 
-    timer_cnt = TIMER_CLK_HZ / (freq_hz * SAMPLES_PER_CYCLE);
+    if(sw & 0x80000000)
+
+        timer_cnt = TIMER_CLK_HZ / (freq_hz * 256);
+
+    else
+
+        timer_cnt = TIMER_CLK_HZ / (freq_hz * 128);
 
     if(timer_cnt < 2) timer_cnt = 2;
 
@@ -408,11 +414,17 @@ void process_fixed_frame(u8 cmd_ch, u8* data)
 
         if(data[0] != 0)
 
-            sw |=  0x80000000;
+            sw |= 0x80000000;
 
         else
 
             sw &= ~0x80000000;
+
+        sw &= ~0x40000000;
+
+        new_freq = freq_hz_a;
+
+        freq_update = 1;
 
         return;
 
@@ -608,21 +620,31 @@ void T0Handler(void)
 
         if(sw & 0x80000000) {
 
-            u8 rv_a = wave_tables[wave_type_a][table_index_a];
+            if(!(sw & 0x40000000)) {
 
-            u8 rv_b = wave_tables[wave_type_b][table_index_a];
+                u8 rv_b = wave_tables[wave_type_b][table_index_a];
 
-            u8 ov_a = (rv_a * amplitude_a) >> 7;
+                u8 ov_b = (rv_b * amplitude_b) >> 7;
 
-            u8 ov_b = (rv_b * amplitude_b) >> 7;
+                dac_write_fast(BUF_CMD, ov_b);
 
-            while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
+                sw |= 0x40000000;
 
-            dac_write_fast(BUF_CMD, ov_b);
+            } else {
 
-            while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
+                u8 rv_a = wave_tables[wave_type_a][table_index_a];
 
-            dac_write_fast(CHA_CMD, ov_a);
+                u8 ov_a = (rv_a * amplitude_a) >> 7;
+
+                dac_write_fast(CHA_CMD, ov_a);
+
+                sw &= ~0x40000000;
+
+                table_index_a++;
+
+                if(table_index_a >= SAMPLES_PER_CYCLE) table_index_a = 0;
+
+            }
 
         } else {
 
@@ -632,13 +654,13 @@ void T0Handler(void)
 
             dac_write_fast(CHA_CMD, out_val);
 
+
+
+            table_index_a++;
+
+            if(table_index_a >= SAMPLES_PER_CYCLE) table_index_a = 0;
+
         }
-
-
-
-        table_index_a++;
-
-        if(table_index_a >= SAMPLES_PER_CYCLE) table_index_a = 0;
 
 
 
