fb091d8e2f4c8a8d4801f36c961f58dcc9c517a6
liyineng <liyineng@github.com>
2026-06-10 17:40:27 +0800

    revert: back to stable 2330ebe, main-loop approach had sw RMW race

--- zhxm/zhxm.sdk/u/src/helloworld.c
+++ zhxm/zhxm.sdk/u/src/helloworld.c
@@ -361,7 +361,13 @@ u32 calc_load_value(u32 freq_hz)
 
     if(freq_hz > 20000) freq_hz = 20000;
 
-    timer_cnt = TIMER_CLK_HZ / (freq_hz * SAMPLES_PER_CYCLE);
+    if(sw & 0x80000000)
+
+        timer_cnt = TIMER_CLK_HZ / (freq_hz * 256);
+
+    else
+
+        timer_cnt = TIMER_CLK_HZ / (freq_hz * 128);
 
     if(timer_cnt < 2) timer_cnt = 2;
 
@@ -408,11 +414,17 @@ void process_fixed_frame(u8 cmd_ch, u8* data)
 
         if(data[0] != 0)
 
-            sw |=  0x80000000;
+            sw |= 0x80000000;
 
         else
 
             sw &= ~0x80000000;
+
+        sw &= ~0x40000000;
+
+        new_freq = freq_hz_a;
+
+        freq_update = 1;
 
         return;
 
@@ -602,15 +614,55 @@ void T0Handler(void)
 
             freq_update = 0;
 
-            sw |= 0x20000000;
+            Xil_Out32(TIMER_BASE + XTC_TLR_OFFSET, calc_load_value(new_freq));
 
         }
 
-        table_index_a++;
+        if(sw & 0x80000000) {
 
-        if(table_index_a >= SAMPLES_PER_CYCLE) table_index_a = 0;
+            if(!(sw & 0x40000000)) {
 
-        sw |= 0x40000000;
+                u8 rv_b = wave_tables[wave_type_b][table_index_a];
+
+                u8 ov_b = (rv_b * amplitude_b) >> 7;
+
+                dac_write_fast(BUF_CMD, ov_b);
+
+                sw |= 0x40000000;
+
+            } else {
+
+                u8 rv_a = wave_tables[wave_type_a][table_index_a];
+
+                u8 ov_a = (rv_a * amplitude_a) >> 7;
+
+                dac_write_fast(CHA_CMD, ov_a);
+
+                sw &= ~0x40000000;
+
+                table_index_a++;
+
+                if(table_index_a >= SAMPLES_PER_CYCLE) table_index_a = 0;
+
+            }
+
+        } else {
+
+            u8 raw_val = wave_tables[wave_type_a][table_index_a];
+
+            u8 out_val = (raw_val * amplitude_a) >> 7;
+
+            dac_write_fast(CHA_CMD, out_val);
+
+
+
+            table_index_a++;
+
+            if(table_index_a >= SAMPLES_PER_CYCLE) table_index_a = 0;
+
+        }
+
+
 
         Xil_Out32(TIMER_BASE + XTC_TCSR_OFFSET, tcsr | (1<<8));
 
@@ -739,71 +791,9 @@ int main(void)
 
     while(1){
 
-        if(sw & 0x20000000) {
+//    for(int i=0;i<10000;i++);
 
-            sw &= ~0x20000000;
-
-            Xil_Out32(TIMER_BASE + XTC_TLR_OFFSET, calc_load_value(new_freq));
-
-        }
-
-        if(sw & 0x40000000) {
-
-            sw &= ~0x40000000;
-
-            if(sw & 0x80000000) {
-
-                u8 rv_a = wave_tables[wave_type_a][table_index_a];
-
-                u8 rv_b = wave_tables[wave_type_b][table_index_a];
-
-                u8 ov_a = (rv_a * amplitude_a) >> 7;
-
-                u8 ov_b = (rv_b * amplitude_b) >> 7;
-
-                u16 tx_b = BUF_CMD | ((u16)ov_b << 4);
-
-                u16 tx_a = CHA_CMD | ((u16)ov_a << 4);
-
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-
-                Xil_Out32(SPI_BASE + SPIDTR, tx_b);
-
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
-
-                {
-
-                    volatile u32 _d;
-
-                    for(_d = 0; _d < 50; _d++);
-
-                }
-
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-
-                Xil_Out32(SPI_BASE + SPIDTR, tx_a);
-
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
-
-            } else {
-
-                u8 raw_val = wave_tables[wave_type_a][table_index_a];
-
-                u8 out_val = (raw_val * amplitude_a) >> 7;
-
-                dac_write_fast(CHA_CMD, out_val);
-
-            }
-
-        }
+//
 
     };
 
