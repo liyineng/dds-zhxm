652a34b8a2240ce952d590f00b335b181db80be2
liyineng <liyineng@github.com>
2026-06-10 18:45:17 +0800

    debug: minimal dual dac_write_fast (no CS toggle, no TX Empty wait)

--- zhxm/zhxm.sdk/dds/src/helloworld.c
+++ zhxm/zhxm.sdk/dds/src/helloworld.c
@@ -363,25 +363,10 @@ int main(void)
             {
                 u8 rv_a = wave_tables[wave_type_a][table_index_a];
                 u8 rv_b = wave_tables[wave_type_b][table_index_a];
-                u16 tx_b = BUF_CMD | ((u16)(((rv_b * amplitude_b) >> 7)) << 4);
-                u16 tx_a = CHA_CMD | ((u16)(((rv_a * amplitude_a) >> 7)) << 4);
-
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-                Xil_Out32(SPI_BASE + SPIDTR, tx_b);
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
-
-                {
-                    volatile u32 _d;
-                    for(_d = 0; _d < 1000; _d++);
-                }
-
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-                Xil_Out32(SPI_BASE + SPIDTR, tx_a);
-                while(!(Xil_In32(SPI_BASE + SPISR) & (1<<2)));
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
+                u8 ov_a = (rv_a * amplitude_a) >> 7;
+                u8 ov_b = (rv_b * amplitude_b) >> 7;
+                dac_write_fast(BUF_CMD, ov_b);
+                dac_write_fast(CHA_CMD, ov_a);
             }
         }
     };
