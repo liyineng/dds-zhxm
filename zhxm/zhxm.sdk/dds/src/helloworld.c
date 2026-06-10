e041de9650d45c87d7f2112d32107416393da53d
liyineng <liyineng@github.com>
2026-06-10 19:03:42 +0800

    debug: single CHA_CMD with manual CS to verify SPI mode works

--- zhxm/zhxm.sdk/dds/src/helloworld.c
+++ zhxm/zhxm.sdk/dds/src/helloworld.c
@@ -360,31 +360,15 @@ int main(void)
         uart_poll();
         if(dac_pending) {
             dac_pending = 0;
+            u8 raw_val = wave_tables[wave_type_a][table_index_a];
+            u8 out_val = (raw_val * amplitude_a) >> 7;
+            Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
+            dac_write_fast(CHA_CMD, out_val);
             {
-                u8 rv_a = wave_tables[wave_type_a][table_index_a];
-                u8 rv_b = wave_tables[wave_type_b][table_index_a];
-                u8 ov_a = (rv_a * amplitude_a) >> 7;
-                u8 ov_b = (rv_b * amplitude_b) >> 7;
-
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-                dac_write_fast(CHB_CMD, ov_b);
-                {
-                    volatile u32 _d;
-                    for(_d = 0; _d < 2000; _d++);
-                }
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
-                {
-                    volatile u32 _d;
-                    for(_d = 0; _d < 200; _d++);
-                }
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-                dac_write_fast(CHA_CMD, ov_a);
-                {
-                    volatile u32 _d;
-                    for(_d = 0; _d < 2000; _d++);
-                }
-                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
+                volatile u32 _d;
+                for(_d = 0; _d < 2000; _d++);
             }
+            Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
         }
     };
     return 0;
