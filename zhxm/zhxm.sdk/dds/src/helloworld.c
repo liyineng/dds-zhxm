cc132560f42954a0f1ffd098d1e532d975cf3062
liyineng <liyineng@github.com>
2026-06-10 19:08:32 +0800

    debug: CHB_CMD (mode 00) + CHA_CMD (mode 10) with longer CS delay 5000

--- zhxm/zhxm.sdk/dds/src/helloworld.c
+++ zhxm/zhxm.sdk/dds/src/helloworld.c
@@ -360,26 +360,31 @@ int main(void)
         uart_poll();
         if(dac_pending) {
             dac_pending = 0;
-            u8 ov = (wave_tables[wave_type_a][table_index_a] * amplitude_a) >> 7;
+            {
+                u8 rv_a = wave_tables[wave_type_a][table_index_a];
+                u8 rv_b = wave_tables[wave_type_b][table_index_a];
+                u8 ov_a = (rv_a * amplitude_a) >> 7;
+                u8 ov_b = (rv_b * amplitude_b) >> 7;
 
-            Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-            dac_write_fast(CHA_CMD, ov);
-            {
-                volatile u32 _d;
-                for(_d = 0; _d < 2000; _d++);
+                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
+                dac_write_fast(CHB_CMD, ov_b);
+                {
+                    volatile u32 _d;
+                    for(_d = 0; _d < 2000; _d++);
+                }
+                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
+                {
+                    volatile u32 _d;
+                    for(_d = 0; _d < 5000; _d++);
+                }
+                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
+                dac_write_fast(CHA_CMD, ov_a);
+                {
+                    volatile u32 _d;
+                    for(_d = 0; _d < 2000; _d++);
+                }
+                Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
             }
-            Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
-            {
-                volatile u32 _d;
-                for(_d = 0; _d < 5000; _d++);
-            }
-            Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFE);
-            dac_write_fast(CHA_CMD, ov);
-            {
-                volatile u32 _d;
-                for(_d = 0; _d < 2000; _d++);
-            }
-            Xil_Out32(SPI_BASE + SPISSR, 0xFFFFFFFF);
         }
     };
     return 0;
