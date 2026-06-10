6fc6873eea835359af1364c6d47b41e9e1b68e52
liyineng <liyineng@github.com>
2026-06-10 16:56:16 +0800

    Revert "feat: init both DAC channels to sine at startup"
    This reverts commit af611aa1848d5d29923df824c3dbc83e7b5b91d5.

--- zhxm/zhxm.sdk/u/src/helloworld.c
+++ zhxm/zhxm.sdk/u/src/helloworld.c
@@ -735,16 +735,6 @@ int main(void)
 
     spi_init();
 
-    {
-
-        u8 raw = sine_table[0];
-
-        dac_write_fast(CHB_CMD, (raw * amplitude_b) >> 7);
-
-        dac_write_fast(CHA_CMD, (raw * amplitude_a) >> 7);
-
-    }
-
     gpio_init();
 
     uart_init();
